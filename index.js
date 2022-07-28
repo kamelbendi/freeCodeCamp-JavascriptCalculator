5b566997ce14283361f381a1fc12e59699e10fd5
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-07-29 00:48:50 +0200

    adding all of the calculator functionnalities

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 function App(){
 
     const [expression, setExpression] = React.useState("");
-    const [answer, setAnswer] = React.useState(0)
+    const [answer, setAnswer] = React.useState(0);
     const display = (symbol ) => {
-        setExpression( prev => prev + symbol)
+        setExpression( prev => prev + symbol);
+        if(expression[expression.length -1] == "="){
+            if(/[1-9.]/.test(symbol)){
+                setExpression(symbol);
+            }
+            else{
+                setExpression(answer + symbol)
+            }
+        }
     };
 
     const calculate = () => {
-        setAnswer(eval(expression))
+        setAnswer(eval(expression));
+        setExpression( prev => prev + "=")
     };
 
     const allClear = () => {
-
+        setExpression("");
+        setAnswer(0);
     };
 
     const clear = () => {
-        
+        setExpression( prev => prev.split("").slice(0,prev.length - 1).join(""));
+        setAnswer(0);
     };
     return (
         <div className="container">
